4f516ea25c6b44a80de1e5f49bed5c637cfee785
윤성 <yunyul1209@gmail.com>
2022-07-14 16:48:34 +0900

    design : Emotion으로 Reset.css 설정

--- client/layouts/App.tsx
+++ client/layouts/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import loadable from '@loadable/component';
+import { Global } from '@emotion/react';
+import reset from '@utils/Reset';
 
 const LogIn = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LogIn />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
+    <>
+      <Global styles={reset} />
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
   );
 };
 
