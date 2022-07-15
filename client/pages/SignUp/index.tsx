27eaf8aaf7cfe7556cfaa06e320d3b2bcaffbbb3
윤성 <yunyul1209@gmail.com>
2022-07-15 11:52:31 +0900

    feat : Custom Hook 추가

--- client/pages/SignUp/index.tsx
+++ client/pages/SignUp/index.tsx
@@ -1,20 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import { Form, Label, Input, LinkContainer, Button, Header, Error, Container } from './styles';
+import useInput from '@hooks/useInput';
 
 function SignUp() {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [mismatchError, setMismatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback(
     (e) => {
