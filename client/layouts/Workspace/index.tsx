2f88a54dac3545d64bb4a41c74ec53b70a465d04
윤성 <yunyul1209@gmail.com>
2022-07-19 16:17:45 +0900

    feat : Profile 메뉴 추가

--- client/layouts/Workspace/index.tsx
+++ client/layouts/Workspace/index.tsx
@@ -1,26 +1,39 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import axios from 'axios';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import {
+  AddButton,
   Channels,
   Chats,
   Header,
+  LogOutButton,
   MenuScroll,
   ProfileImg,
+  ProfileModal,
   RightMenu,
+  WorkspaceButton,
   WorkspaceName,
   Workspaces,
   WorkspaceWrapper,
 } from '@layouts/Workspace/styles';
 import gravatar from 'gravatar';
 import loadable from '@loadable/component';
+import Menu from '@components/Menu';
+import { IUser } from '@typings/db';
+
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
-const Workspace: FC = () => {
-  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
+const Workspace: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
+
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
     dedupingInterval: 2000,
   });
 
@@ -33,8 +46,16 @@ const Workspace: FC = () => {
       .catch();
   }, []);
 
+  // 유저 프로필 메뉴 Toggle
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
+  // 워크스페이스 생성
+  const onClickCreateWorkspace = useCallback(() => {}, []);
+
   // 로그아웃 성공
-  if (!data) {
+  if (!userData) {
     return <Navigate to="/login" />;
   }
 
@@ -42,25 +63,40 @@ const Workspace: FC = () => {
     <div>
       <Header>
         <RightMenu>
-          <span>
+          <span onClick={onClickUserProfile}>
             <ProfileImg
-              src={gravatar.url(data.email, { s: '28px', d: 'retro' })}
-              alt={data.nickname}
+              src={gravatar.url(userData.email, { s: '28px', d: 'retro' })}
+              alt={userData.nickname}
             />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} onCloseModal={onClickUserProfile}>
+                <ProfileModal>
+                  <img
+                    src={gravatar.url(userData.email, { s: '36px', d: 'retro' })}
+                    alt={userData.nickname}
+                  />
+                  <div>
+                    <span id="profile-name">{userData.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton>LogOut</LogOutButton>
+              </Menu>
+            )}
           </span>
         </RightMenu>
       </Header>
       <button onClick={onLogout}>Logout</button>
       <WorkspaceWrapper>
-        <Workspaces>워크스페이스 탭</Workspaces>
+        <Workspaces></Workspaces>
         <Channels>
           <WorkspaceName>워크스페이스 이름</WorkspaceName>
           <MenuScroll>메뉴자리</MenuScroll>
         </Channels>
         <Chats>
           <Routes>
-            <Route path="/channel/:channel" element={<Channel />} />
-            <Route path="/dm/:id" element={<DirectMessage />} />
+            <Route path="/channel" element={<Channel />} />
+            <Route path="/dm" element={<DirectMessage />} />
           </Routes>
         </Chats>
       </WorkspaceWrapper>
