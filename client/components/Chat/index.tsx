8dcb74d0ae7b827d42a0d2ae3a1ffa06edb1249e
윤성 <yunyul1209@gmail.com>
2022-08-04 11:01:37 +0900

    feat : 날짜 표기를 위한 dayjs 라이브러리 적용

--- client/components/Chat/index.tsx
+++ client/components/Chat/index.tsx
@@ -1,5 +1,6 @@
 import React, { VFC } from 'react';
 import gravatar from 'gravatar';
+import dayjs from 'dayjs';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
 
@@ -17,7 +18,7 @@ const Chat: VFC<Props> = ({ data }) => {
       <div className="chat-text">
         <div className="chat-user">
           <b>{user.nickname}</b>
-          <span>{data.createdAt}</span>
+          <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
         <p>{data.content}</p>
       </div>
